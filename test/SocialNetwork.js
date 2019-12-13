087c15a4536555c1f10e601612b4609e7c793d45
Sergei <sergeisqa@gmail.com>
2019-12-13 13:55:28 -0700

    End of part 3

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -68,6 +68,11 @@ assert.equal(post.author, author, 'author is correct')
 })
 
 it('allows users to tip posts' , async() => {	
+
+let oldAuthorBalance
+oldAuthorBalance = await web3.eth.getBalance(author)
+oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+
 result = await socialNetwork.tipPost(postCount, { from:tipper, value: web3.utils.toWei (`1`, `Ether`)} )
 //SUCCESS
 const event =  result.logs[0].args
@@ -76,6 +81,20 @@ assert.equal(event.content, 'This is my first post', 'content is correct')
 assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
 assert.equal(event.author, author, 'author is correct')
 
+let newAuthorBalance
+newAuthorBalance = await web3.eth.getBalance(author)
+newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+let tipAmount
+tipAmount = web3.utils.toWei('1', 'Ether')
+tipAmount = new web3.utils.BN(tipAmount)
+
+const expectedBalance = oldAuthorBalance.add(tipAmount)
+
+assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+//FAILURE: tries to tip a post that does not exist
+await socialNetwork.tipPost(99, {from: tipper, value:web3.utils.toWei('1', 'Ether')}).should.be.rejected;
 })
 })
 })
