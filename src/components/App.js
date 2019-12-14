78c54a44277308a3b50cefe1ee260855b1b219b0
Sergei <sergeisqa@gmail.com>
2019-12-14 16:07:11 -0700

    make current account visible

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
+import Web3 from  'web3'
 import logo from '../logo.png';
 import './App.css';
 
+
 class App extends Component {
+
+async componentWillMount() {
+await this.loadWeb3()
+await this.loadBlockchainData()
+}
+async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+
+async loadBlockchainData() {
+  const web3 = window.web3 
+  // load account
+  const accounts = await web3.eth.getAccounts()
+  this.setState({account: accounts[0] })
+}
+
+constructor(props) {
+super(props)
+this.state = {
+  account: ''
+}
+}
+
   render() {
     return (
       <div>
@@ -15,7 +50,15 @@ class App extends Component {
           >
             Dapp University
           </a>
-        </nav>
+          <ul className="navbar-nav px-3">
+<li className= "nav-item text-nowrap d-none d-sm-none d-sm-block">
+<small className="text-secondary">
+<small id ="account"> {this.state.account} </small>
+</small>
+</li>
+</ul>
+</nav>
+        
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
